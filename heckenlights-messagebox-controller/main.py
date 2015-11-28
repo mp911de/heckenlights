a22ffa086354c4bd89b7f4e771334a455fb3a5fb
Mark Paluch <mpaluch@paluch.biz>
2015-11-28 20:04:48 +0100

    Fix typo

--- heckenlights-messagebox-controller/main.py
+++ heckenlights-messagebox-controller/main.py
@@ -91,7 +91,7 @@ def main():
             if width is not None:
                 sleep_time = min(width, max(width - 32, 0)) * scrollspeed_ms_per_pixel
                 command_line=  "%s %s -m %d0.0 %s" % (led_matrix_executable, cmdline, scrollspeed_ms_per_pixel * 100, filename)
-                logging.debug("Command Line: " + command_line)
+                logging.debug("Command line: " + command_line)
                 args = shlex.split(command_line)
                 process = subprocess.Popen(args)
                 logging.info("Width: %d px, Sleep-Time %d sec (org %d), File: %s" % (width, sleep_time, (width * scrollspeed_ms_per_pixel), filename))
